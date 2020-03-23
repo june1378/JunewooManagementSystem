3d6d65e4b2a526f814131ed34a5d295dfb8cdb04
user <user@DESKTOP-RVVUKR6>
2020-03-23 16:39:57 +0900

    add menu

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,6 +3,10 @@ package test;
 
 public class HelloWorld {
     public static void main(String[] args) {
+		System.out.println("1. Add student");
+		System.out.println("2. Delete student");
+		System.out.println("3. Edit student");
+		System.out.println("4. View student");
 		System.out.println("5. Exit");
 		
 	}
