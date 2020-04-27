1f2f433bdc949e70714d6740167322d31c6867df
user <user@DESKTOP-RVVUKR6>
2020-04-27 11:26:30 +0900

    한 중 양식 추가 2

---
+++ src/test/Menukind.java
@@ -0,0 +1,8 @@
+package test;
+
+public enum Menukind {
+	중식,
+	양식,
+	한식,
+
+}
---
+++ src/test/양식.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.util.Scanner;
+
+public class 양식 extends Menu {
+	
+	public 양식(Menukind kind) {
+		super(kind); // super은 위 생성자를 뜻함
+	}
+	
+	public void getUserInput(Scanner input) {
+		 System.out.println("order number:"); 
+	        int ordernumber= input.nextInt(); 
+	        this.setOrdernumber(ordernumber);
+	        
+	        System.out.println("food name:");
+	        String foodname= input.next();
+	        this.setFoodname(foodname);
+	        
+	        System.out.println("food price:"); 
+	        int foodprice= input.nextInt();
+	        this.setFoodprice(foodprice);
+	        
+	        char answer = 'x';
+	        while (answer!= 'Y'&& answer != 'N')
+	        {
+	        	System.out.println("do you want to order a beverage? (Y/N)"); 
+	            answer = input.next().charAt(0);
+	            if(answer =='Y') {
+	            	System.out.println("beverage:"); 
+		            String beverage= input.next();
+		            this.setBeverage(beverage);
+		            break;
+		        }
+	            else if (answer =='N') {
+	        	    this.setBeverage("");
+	        	    break;
+	            }
+	            else {
+	        	
+	        }
+	        
+	}
+
+  }
+}
